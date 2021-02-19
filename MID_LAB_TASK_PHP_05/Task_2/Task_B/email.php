1fbab706b9191f5a1b52798985ef70ad23ebf937
Faiaz Ben Reza <faiazbenreza@gmail.com>
2021-02-19 23:11:12 +0600

    task2B

---
+++ MID_LAB_TASK_PHP_05/Task_2/Task_B/email.php
@@ -0,0 +1,32 @@
+<?php
+
+	if(isset($_POST['submit']))
+    {
+
+		$mail 		= $_POST['mail'];
+
+		if($mail == "")
+        {
+			echo "Null submission...";
+		}else{
+			echo "Email: ",$mail;
+		}
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email Input</title>
+</head>
+<body>
+	<form method="post" action="#">
+		<fieldset>
+			<legend>Email</legend>
+			<input type="email" name="mail" value=""> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
